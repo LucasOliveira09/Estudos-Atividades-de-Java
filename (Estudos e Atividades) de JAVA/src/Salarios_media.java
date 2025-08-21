a364b804b30372634b115f7262e0e2cdb1afe3f9
Lucas Oliveira <lucasleiteoliveira09@gmail.com>
2025-08-20 21:31:24 -0300

    Mais

---
+++ (Estudos e Atividades) de JAVA/src/Salarios_media.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+
+public class Salarios_media {
+    public static void main(String[] args) {
+
+        int func1, func2, func3, medias;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Coloque que o primeiro numero para a soma: ");
+        n1 = input.nextInt();
+
+        System.out.println("Coloque que o segundo numero para a soma: ");
+        n2 = input.nextInt();
+
+        System.out.println("Coloque que o terceiro numero para a soma: ");
+        n3 = input.nextInt();
+
+        System.out.println(n1+n2+n3);
+
+
+    }
+}
